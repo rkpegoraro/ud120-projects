222a32696d1f84773a028d6d2024519780bd99e4
Ricardo Pegoraro <knoff@localhost.localdomain>
2021-07-05 17:50:06 -0300

    lesson 8 - outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -14,7 +14,10 @@ def outlierCleaner(predictions, ages, net_worths):
     cleaned_data = []
 
     ### your code goes here
+    for a, n, p in zip(ages, net_worths, predictions) :
+        #print a[0], " - ", n[0], " - ", p[0]
+        cleaned_data.append((a[0], n[0], abs(p[0] - n[0])))
 
-    
+    cleaned_data = sorted(cleaned_data, key=lambda x: x[2])[:81]
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,16 +25,15 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+print "Slope: ", reg.coef_
+print "Intercept: ", reg.intercept_
 
-
-
-
-
-
-
-
+print "Score with test data: ", reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -52,9 +51,6 @@ try:
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
 
 
 
@@ -77,7 +73,9 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
-
+    print "Slope cleaned data: ", reg.coef_
+    print "Intercept cleaned data: ", reg.intercept_
+    print "Score (cleaned data) with test data: ", reg.score(ages_test, net_worths_test)
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
